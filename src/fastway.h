f2945bb63f6924e689eff618b5ba3ca3ebd51ecb
Rafael <77973453+Unbreak4ble@users.noreply.github.com>
2021-09-13 23:04:43 -0400

    Create fastway.h

---
+++ src/fastway.h
@@ -0,0 +1,110 @@
+#pragma once
+#include <vector>
+#include <iostream>
+#include <fstream>
+#include "components.h"
+
+/*
+codes:
+0 : wall
+1 : way
+2 : begin/actually position
+3 : end
+4 : passed
+*/
+
+namespace FastWay {
+  
+   std::vector<std::vector<components::xy>> start(std::vector<std::vector<int>> lab){
+     auto newlab = lab;
+     auto SE = components::getSE(lab);
+     auto prox = components::getAproxIdx(lab, 1, SE[0]);
+     bool stop = prox.idx.x == -1 && prox.idx.y == -1;
+     std::vector<std::vector<components::xy>> results;
+     std::vector<components::xy> passed;
+     std::string reto = stop == false ? prox.opt : "";
+     bool needReturn = false;
+     int i=0;
+ 
+     
+     while(!stop){
+       
+       i++;
+       if(i>20)
+       {
+         std::cout << "aborted" << std::endl;
+         stop = true;
+       }
+       
+       auto idx3 = components::getAproxIdx(newlab, 3, prox.idx);
+     
+       passed.push_back(prox.idx);
+       
+       if(!(idx3.idx.x == -1 && idx3.idx.y == -1)){
+       results.push_back(passed);
+       stop = true;
+       }
+       
+       if(!needReturn){
+          components::change(newlab, prox.idx, 4);
+         
+            auto idx = components::walk(newlab, prox.idx, reto);
+            std::cout << prox.opt << " | " << prox.idx.x << "/" << prox.idx.y  << " - " << idx.x << "/" << idx.y << " | " << reto << std::endl;
+        
+           if(!(idx.x == -1 && idx.y == -1) && components::getPos(newlab, idx) == 1){
+           prox.idx = idx;
+           prox.opt = reto;
+           }else
+           if(!(prox.idx.x == -1 && prox.idx.y == -1)){
+           prox = components::getAproxIdx(newlab, 1, prox.idx);
+           reto = prox.opt;
+           }
+           else{
+           needReturn = true;
+           prox = components::getAproxIdx(newlab, 1, prox.idx);
+           }
+           
+         
+      
+       }else{
+          std::cout << "need return" << std::endl;
+          
+          for(int i=passed.size()-1; i>=0 && needReturn == true; i--){
+          std::cout << "xy: " << passed[i].x << "/" << passed[i].y << std::endl;
+           prox = components::getAproxIdx(newlab, 1, passed[i]);
+           
+           if(!(prox.idx.x == -1 && prox.idx.y == -1))
+           {
+             reto = prox.opt;
+             passed.push_back(prox.idx);
+             components::change(newlab, prox.idx, 4);
+             needReturn = false;
+           }else
+            passed.erase(passed.end()-1);
+            
+          }
+          
+         stop = needReturn == true;
+       }
+     }
+     
+     components::print(newlab);
+     std::cout << "-----------" << std::endl;
+     
+     return results;
+   }
+  
+   std::vector<std::vector<int>> run(std::vector<std::vector<int>> lab){
+     auto results = start(lab);
+     components::replaceAll(lab, results, 4);
+     
+     return lab;
+   }
+   
+   std::vector<std::vector<int>> run(std::string filename){
+     std::string content = components::readFile(filename);
+     auto lab = components::interpret(content);
+     return run(lab);
+   }
+
+}
